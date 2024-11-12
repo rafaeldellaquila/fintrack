6fd5f72e3b9fb448163696491bd1428888fe6e9e
rafaeldellaquila <rafael@dellaquila.dev>
2024-11-12 16:22:00 -0300

    :sparkles: eslint configurations

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ['@nuxt/ui', '@nuxt/eslint'],
+	devtools: { enabled: true },
+	future: {
+		compatibilityVersion: 4,
+	},
+	compatibilityDate: '2024-04-03',
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+				semi: true,
+				arrowParens: true,
+				quotes: 'single',
+				quoteProps: 'consistent-as-needed',
+				commaDangle: 'only-multiline'
+			}
+		}
+	},
+});
