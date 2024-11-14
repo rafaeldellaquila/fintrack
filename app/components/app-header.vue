9bad34f4a3fdcf5a7641a046494c7b9e4d87619b
rafaeldellaquila <rafael@dellaquila.dev>
2024-11-14 17:07:30 -0300

    :sparkles: start the new components and layout/pages

---
+++ app/components/app-header.vue
@@ -0,0 +1,24 @@
+<template>
+	<header class="flex justify-between items-center">
+		<NuxtLink
+			to="/"
+			class="text-xl font-bold"
+		>
+			Fintrack
+		</NuxtLink>
+		<div>
+			<UAvatar
+				src="https://avatars.githubusercontent.com/u/739984?v=4"
+				alt="Logged User {user_name} avatar"
+			/>
+		</div>
+	</header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<section class="flex items-center justify-between mb-10">
+		<h1 class="text-4xl font-extrabold">
+			Summary
+		</h1>
+		<div>
+			<USelect
+				v-model="viewSelected"
+				:options="transactionViewOptions"
+			/>
+		</div>
+	</section>
+
+	<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+		<FinancialTrend />
+		<FinancialTrend />
+		<FinancialTrend />
+		<FinancialTrend />
+	</section>
+</template>
+
+<script setup lang="ts">
+import FinancialTrend from '~/components/financial-trend.vue';
+import { transactionViewOptions } from '~/utils/constants';
+
+const viewSelected = ref(transactionViewOptions[1]);
+</script>
+
+<style></style>
