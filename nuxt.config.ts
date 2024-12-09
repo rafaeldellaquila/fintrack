d59c3644ce806781ffd8445b99485748b6be5e09
rafaeldellaquila <rafael@dellaquila.dev>
2024-12-09 17:59:37 -0300

    :arrow_up: Update nuxt and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxtjs/supabase'],
+	modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
 	devtools: { enabled: true },
 	future: {
 		compatibilityVersion: 4,
 	},
 	compatibilityDate: '2024-04-03',
+	postcss: {
+		plugins: {
+			'postcss-import': {},
+			'tailwindcss/nesting': {},
+			'tailwindcss': {},
+			'autoprefixer': {},
+		}
+	},
 	eslint: {
 		config: {
 			stylistic: {
