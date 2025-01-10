a58188a9233cf9d677b910b31d9b56a65e784a2d
rafaeldellaquila <rafael@dellaquila.dev>
2025-01-10 14:45:47 -0300

    :arrow_up: upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,4 +29,20 @@ export default defineNuxtConfig({
 	supabase: {
 		redirect: false
 	},
+	tailwindcss: {
+		config: {
+			content: [
+				'./app/components/**/*.{js,vue,ts}',
+				'./app/layouts/**/*.vue',
+				'./app/pages/**/*.vue',
+				'./app/plugins/**/*.{js,ts}',
+				'./app/app.vue',
+				'./app/error.vue',
+			],
+			theme: {
+				extend: {},
+			},
+			plugins: [],
+		}
+	}
 });
